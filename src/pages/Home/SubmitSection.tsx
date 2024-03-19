667310c4624a0fc7acde868a9c39984d3a3ba91b
tianmarillio <tiansyah.marillio@gmail.com>
2024-03-19 18:15:55 +0700

    feat: update font color, add button hover state

--- src/pages/Home/SubmitSection.tsx
+++ src/pages/Home/SubmitSection.tsx
@@ -1,7 +1,9 @@
 const SubmitSection = () => {
   return (
     <div className="h-[60px] px-[15px] py-[10px]">
-      <button className="size-full rounded bg-[#FFCE22]">Done</button>
+      <button className="size-full rounded bg-[#FFCE22] hover:bg-[#FFD84D]">
+        Done
+      </button>
     </div>
   );
 };
